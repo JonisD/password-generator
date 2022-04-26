9d6e5a71e1113d0dcdc7c2a59dccdedcbb17c1bc
jonisd <jonlls@yahoo.co.uk>
2022-04-26 09:02:05 +0100

    bug fix on console

--- script.js
+++ script.js
@@ -60,3 +60,28 @@ const createRandomPassword = (passwordLength, passwordCriteria) => {
 
   return random_password;
 };
+
+// main function to generate the random password
+const generatePassword = () => {
+  // get the password length
+  const passwordLength = getPasswordLength();
+
+  // get the password criteria
+  const passwordCriteria = getPasswordCriteria();
+
+  // create random password
+  const password = createRandomPassword(passwordLength, passwordCriteria);
+
+  return password;
+};
+
+// Write password to the #password input
+const writePassword = () => {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
